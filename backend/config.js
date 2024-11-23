a0e24868fd7b2ebb7fe0f727715747924d921133
Bakshish Singh <bakshish10621@gmail.com>
2024-11-23 23:59:32 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://bakshish10621:Dt8kKA7hrd1REI77@cluster0.fecizke.mongodb.net/databaseName?retryWrites=true&w=majority';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial 
